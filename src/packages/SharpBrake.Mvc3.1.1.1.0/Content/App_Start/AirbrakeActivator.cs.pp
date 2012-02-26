75cfe96fde37c4bc5037eca854b384191af6b53c
Leonardo Cardoso <kibiluzbad@hotmail.com>
2012-02-26 12:01:34 -0300

    Adding airbrake.

---
+++ src/packages/SharpBrake.Mvc3.1.1.1.0/Content/App_Start/AirbrakeActivator.cs.pp
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using SharpBrake.Mvc3;
+using WebActivator;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.AirbrakeActivator), "Start")]
+namespace $rootnamespace$.App_Start
+{
+    public static class AirbrakeActivator
+    {
+        public static void Start()
+        {
+            GlobalFilters.Filters.Add(new AirbrakeNoticeFilter());
+        }
+    }
+}
